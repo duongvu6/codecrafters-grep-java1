ab34b3c1c953e57cb5dddbc152e7c7c2bf491df7
duongvct <vucongtuanduong2409@gmail.com>
2025-09-06 14:23:25 +0700

    Positive Character Groups #tl6

--- src/main/java/service/PatternMatcher.java
+++ src/main/java/service/PatternMatcher.java
@@ -10,6 +10,13 @@ public class PatternMatcher {
         } else if (pattern.equals("\\w")) {
           return inputLine.chars().anyMatch(c -> Character.isLetterOrDigit(c) || c == '_');
         } else {
+            if(pattern.charAt(0) == '[' && pattern.charAt(pattern.length() - 1) == ']') {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 1; i < pattern.length() - 1; i++) {
+                    sb.append(pattern.charAt(i));
+                }
+                return inputLine.contentEquals(sb);
+            }
             throw new RuntimeException("Unhandled pattern: " + pattern);
         }
     }
