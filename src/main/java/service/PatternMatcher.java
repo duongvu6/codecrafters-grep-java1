ffc3bff7cb6272ca9185ac786d468701ca9cda2b
duongvct <vucongtuanduong2409@gmail.com>
2025-09-06 14:15:16 +0700

    Match word characters #mr9

--- src/main/java/service/PatternMatcher.java
+++ src/main/java/service/PatternMatcher.java
@@ -6,7 +6,7 @@ public class PatternMatcher {
         if (pattern.length() == 1) {
             return inputLine.contains(pattern);
         } else if (pattern.equals("\\d")) {
-            return inputLine.chars().allMatch(Character::isDigit);
+            return inputLine.chars().anyMatch(Character::isDigit);
         } else if (pattern.equals("\\w")) {
           return inputLine.chars().anyMatch(c -> Character.isLetterOrDigit(c) || c == '_');
         } else {
