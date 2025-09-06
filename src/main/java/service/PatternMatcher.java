61c1d99e54efa91d8bc6482eef2ad915d07e5167
duongvct <vucongtuanduong2409@gmail.com>
2025-09-06 14:28:23 +0700

    Positive Character Groups #tl6

--- src/main/java/service/PatternMatcher.java
+++ src/main/java/service/PatternMatcher.java
@@ -9,14 +9,10 @@ public class PatternMatcher {
             return inputLine.chars().anyMatch(Character::isDigit);
         } else if (pattern.equals("\\w")) {
           return inputLine.chars().anyMatch(c -> Character.isLetterOrDigit(c) || c == '_');
+        } else if (pattern.startsWith("[") && pattern.endsWith("]")) {
+            String sub = pattern.substring(1, pattern.length() - 1);
+            return inputLine.chars().anyMatch(c -> sub.indexOf(c) != -1);
         } else {
-            if(pattern.charAt(0) == '[' && pattern.charAt(pattern.length() - 1) == ']') {
-                StringBuilder sb = new StringBuilder();
-                for (int i = 1; i < pattern.length() - 1; i++) {
-                    sb.append(pattern.charAt(i));
-                }
-                return inputLine.contentEquals(sb);
-            }
             throw new RuntimeException("Unhandled pattern: " + pattern);
         }
     }
